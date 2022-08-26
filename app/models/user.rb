959931daa0a58aad38d75c025640490322750de2
TakumaYmamoto <104744174+TakumaYmamoto@users.noreply.github.com>
2022-08-26 14:18:09 +0900

    ユーザー確認修正点４

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname,            presence: true, length: { maximum: 6 }
-  validates :last_name,           presence: true
-  validates :first_name,          presence: true
   validates :last_name_kana,      presence: true,format: {with: /\A[ァ-ヶー－]+\z/}
   validates :first_name_kana,     presence: true,format: {with: /\A[ァ-ヶー－]+\z/}
   validates :birth_date,          presence: true
