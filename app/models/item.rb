4790e17921e9880a69ca2515894fcc6b6e36f6d6
TakumaYmamoto <104744174+TakumaYmamoto@users.noreply.github.com>
2022-09-11 12:31:56 +0900

    商品出品修正４

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,7 +8,7 @@ class Item < ApplicationRecord
   belongs_to :shipping_fee
   belongs_to :prefecture
   belongs_to :shipping_time_number
-  has_one :order
+  #has_one :order
 
   validates :item_name,                presence: true
   validates :item_description,         presence: true
@@ -17,5 +17,4 @@ class Item < ApplicationRecord
   validates :shipping_fee_id,          presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :prefecture_id,            presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :shipping_time_number_id,  presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :price,                    presence: true, format: { with: /\A[0-9]+\z/ }, inclusion: { in: 300..9_999_999 }
-end
+  validates :price,                    presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
