3b9bc2a72d3bf1a4cd865b8bf243ef4ce47ab116
TakumaYmamoto <104744174+TakumaYmamoto@users.noreply.github.com>
2022-09-13 15:32:37 +0900

    商品削除機能修正３

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show,]
   before_action :set_item, only: [:show, :edit, :update,:destroy]
-
+  before_action :redirect_root, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -53,4 +53,11 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+    end
+
+    def redirect_root
+      unless 
+        current_user.id == @item.user_id
+        redirect_to root_path
+      end
     end
