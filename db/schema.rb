ae4b2df14ae44b352c64feba4462211db3efe35c
TakumaYmamoto <104744174+TakumaYmamoto@users.noreply.github.com>
2022-09-28 15:41:51 +0900

    修正４

--- db/schema.rb
+++ db/schema.rb
@@ -78,7 +78,7 @@ ActiveRecord::Schema.define(version: 2022_09_14_030818) do
     t.string "first_name", null: false
     t.string "last_name_kana", null: false
     t.string "first_name_kana", null: false
-    t.string "birth_date", null: false
+    t.date "birth_date", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
